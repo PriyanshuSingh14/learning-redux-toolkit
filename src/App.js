b2c08f3cf4a9d5c7ab8b6df36eccc31f35341212
PriyanshuSingh14 <priyanshu.priye0201@gmail.com>
2024-05-11 17:33:24 +0530

    created a master branch

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+
+function App() {
+  
+  return (
+    <Provider store={store}>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </Provider>
+  )
+}
+
+export default App
